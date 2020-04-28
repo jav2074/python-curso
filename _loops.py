e5b07b8a8875fed648aaf9988e059ed52964d0b9
jav2074 <jav.electrochance@gmail.com>
2020-04-27 21:47:37 -0300

    Add loops & conditionals

--- _loops.py
+++ _loops.py
@@ -1,9 +1,11 @@
 print('########################################################################')
+print('LOOPs')
 collection = ['Enrique', 'Franco', 'Alexia', 'Diego', 'Teko']
-
 for i in collection:
 	if i == 'Diego':
 		break
-	print(i)	
-
+	print(i)
+print('########################################################################')
+for j in collection:
+	print(f'indice: {collection.index(j)} - nombre: {j}')
 print('########################################################################')
